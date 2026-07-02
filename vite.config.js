775ed8bbc86b69831805351da367bd5181be070b
swinston07 <sterlingwinston83@gmail.com>
2026-07-02 11:39:34 -0700

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/robofriends2/'
+  base: '/robofriends2/',
+  build: {
+    cssMinify: 'esbuild' // Bypasses the strict LightningCSS error
+  }
 })
